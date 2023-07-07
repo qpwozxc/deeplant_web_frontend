2d6f0703ad2837a91d54894c54f4b3e5eb31a58d
lIlIIIlIIIl <102349883+lIlIIIlIIIl@users.noreply.github.com>
2023-07-07 18:11:13 +0900

    added app,sidebar,users

--- Backend/frontend/src/components/Base/Sidebar.js
+++ Backend/frontend/src/components/Base/Sidebar.js
@@ -13,180 +13,182 @@ import ListItemText from '@mui/material/ListItemText';
 import Badge from '@mui/material/Badge';
 import MuiDrawer from '@mui/material/Drawer';
 import ListSubheader from '@mui/material/ListSubheader';
-import MenuIcon from '@mui/icons-material/Menu';
-import NotificationsIcon from '@mui/icons-material/Notifications';
-import DashboardIcon from '@mui/icons-material/Dashboard';
-import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
-import PeopleIcon from '@mui/icons-material/People';
-import BarChartIcon from '@mui/icons-material/BarChart';
-import LayersIcon from '@mui/icons-material/Layers';
-import AssignmentIcon from '@mui/icons-material/Assignment';
-import List from '@mui/material/List';
-import MuiAppBar from '@mui/material/AppBar';
-import Divider from '@mui/material/Divider';
-import IconButton from '@mui/material/IconButton';
-import ChevronLeftIcon from '@mui/icons-material/ChevronLeft';
-import Toolbar from '@mui/material/Toolbar';
-import LogoutIcon from '@mui/icons-material/Logout';
+import MenuIcon from "@mui/icons-material/Menu";
+import AssignmentIcon from "@mui/icons-material/Assignment";
+import List from "@mui/material/List";
+import MuiAppBar from "@mui/material/AppBar";
+import Divider from "@mui/material/Divider";
+import IconButton from "@mui/material/IconButton";
+import ChevronLeftIcon from "@mui/icons-material/ChevronLeft";
+import Toolbar from "@mui/material/Toolbar";
+import LogoutIcon from "@mui/icons-material/Logout";
 import { signOut } from "firebase/auth";
-import { useNavigate } from 'react-router-dom';
+import { useNavigate } from "react-router-dom";
 import { auth } from "../../firebase-config";
-import DataThresholdingIcon from '@mui/icons-material/DataThresholding';
-import StackedLineChartIcon from '@mui/icons-material/StackedLineChart';
-import GroupIcon from '@mui/icons-material/Group';
+import DataThresholdingIcon from "@mui/icons-material/DataThresholding";
+import StackedLineChartIcon from "@mui/icons-material/StackedLineChart";
+import GroupIcon from "@mui/icons-material/Group";
+import HomeIcon from "@mui/icons-material/Home";
 
 const mainListItems = (
-    <React.Fragment>
-      <ListItemButton >
+  <React.Fragment>
+    <ListItemButton>
+      <ListItemIcon>
+        <HomeIcon />
+      </ListItemIcon>
+      <Button component={Link} to="/MainPage">
+        <ListItemText primary="홈" />
+      </Button>
+    </ListItemButton>
+    <ListItemButton>
+      <ListItemIcon>
+        <DataThresholdingIcon />
+      </ListItemIcon>
+      <Button component={Link} to="/Home">
+        <ListItemText primary="데이터 조회" />
+      </Button>
+    </ListItemButton>
+    <ListItemButton>
+      <ListItemIcon>
+        <StackedLineChartIcon />
+      </ListItemIcon>
+      <Button component={Link} to="/stats">
+        <ListItemText primary="통계 조회" />
+      </Button>
+    </ListItemButton>
+    <ListItemButton>
+      <ListItemIcon>
+        <GroupIcon />
+      </ListItemIcon>
+      <Button component={Link} to="/UserManagement">
+        <ListItemText primary="사용자관리" />
+      </Button>
+    </ListItemButton>
+  </React.Fragment>
+);
 
-        <ListItemIcon>
-          <DataThresholdingIcon />
-        </ListItemIcon>
-            <Button component={Link} to="/Home" >
-                <ListItemText primary="데이터 조회" />
-            </Button>
-      </ListItemButton>
-      <ListItemButton>
-        <ListItemIcon>
-          <StackedLineChartIcon />
-        </ListItemIcon>
-            <Button component={Link} to="/stats" >
-                <ListItemText primary="통계 조회" />
-            </Button>
-      </ListItemButton>
-      <ListItemButton>
-        <ListItemIcon>
-          <GroupIcon />
-        </ListItemIcon>
-            <Button component={Link} to="/UserManagement" >
-                <ListItemText primary="사용자관리" />
-            </Button>
-      </ListItemButton>
-    </React.Fragment>
-  );
-  
- const secondaryListItems = (
-    <React.Fragment>
-      <ListSubheader component="div" inset>
-        Saved reports
-      </ListSubheader>
-      <ListItemButton>
-        <ListItemIcon>
-          <AssignmentIcon />
-        </ListItemIcon>
-        <ListItemText primary="Current month" />
-      </ListItemButton>
-      <ListItemButton>
-        <ListItemIcon>
-          <AssignmentIcon />
-        </ListItemIcon>
-        <ListItemText primary="Last quarter" />
-      </ListItemButton>
-      <ListItemButton>
-        <ListItemIcon>
-          <AssignmentIcon />
-        </ListItemIcon>
-        <ListItemText primary="Year-end sale" />
-      </ListItemButton>
-    </React.Fragment>
-  );
-  
-  const drawerWidth = 240;
-  const defaultTheme = createTheme();
-  const Drawer = styled(MuiDrawer, { shouldForwardProp: (prop) => prop !== 'open' })(
-    ({ theme, open }) => ({
-      '& .MuiDrawer-paper': {
-        position: 'relative',
-        whiteSpace: 'nowrap',
-        width: drawerWidth,
-        transition: theme.transitions.create('width', {
-          easing: theme.transitions.easing.sharp,
-          duration: theme.transitions.duration.enteringScreen,
-        }),
-        boxSizing: 'border-box',
-        ...(!open && {
-          overflowX: 'hidden',
-          transition: theme.transitions.create('width', {
-            easing: theme.transitions.easing.sharp,
-            duration: theme.transitions.duration.leavingScreen,
-          }),
-          width: theme.spacing(7),
-          [theme.breakpoints.up('sm')]: {
-            width: theme.spacing(9),
-          },
-        }),
+const secondaryListItems = (
+  <React.Fragment>
+    <ListSubheader component="div" inset>
+      Saved reports
+    </ListSubheader>
+    <ListItemButton>
+      <ListItemIcon>
+        <AssignmentIcon />
+      </ListItemIcon>
+      <ListItemText primary="Current month" />
+    </ListItemButton>
+    <ListItemButton>
+      <ListItemIcon>
+        <AssignmentIcon />
+      </ListItemIcon>
+      <ListItemText primary="Last quarter" />
+    </ListItemButton>
+    <ListItemButton>
+      <ListItemIcon>
+        <AssignmentIcon />
+      </ListItemIcon>
+      <ListItemText primary="Year-end sale" />
+    </ListItemButton>
+  </React.Fragment>
+);
+
+const drawerWidth = 240;
+const defaultTheme = createTheme();
+
+const Drawer = styled(MuiDrawer, {
+  shouldForwardProp: (prop) => prop !== "open",
+})(({ theme, open }) => ({
+  "& .MuiDrawer-paper": {
+    position: "relative",
+    whiteSpace: "nowrap",
+    width: drawerWidth,
+    transition: theme.transitions.create("width", {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.enteringScreen,
+    }),
+    boxSizing: "border-box",
+    ...(!open && {
+      overflowX: "hidden",
+      transition: theme.transitions.create("width", {
+        easing: theme.transitions.easing.sharp,
+        duration: theme.transitions.duration.leavingScreen,
+      }),
+      width: theme.spacing(7),
+      [theme.breakpoints.up("sm")]: {
+        width: theme.spacing(9),
       },
     }),
-  );
+  },
+}));
 
-  const AppBar = styled(MuiAppBar, {
-    shouldForwardProp: (prop) => prop !== 'open',
-  })(({ theme, open }) => ({
-    zIndex: theme.zIndex.drawer + 1,
-    transition: theme.transitions.create(['width', 'margin'], {
+const AppBar = styled(MuiAppBar, {
+  shouldForwardProp: (prop) => prop !== "open",
+})(({ theme, open }) => ({
+  zIndex: theme.zIndex.drawer + 1,
+  transition: theme.transitions.create(["width", "margin"], {
+    easing: theme.transitions.easing.sharp,
+    duration: theme.transitions.duration.leavingScreen,
+  }),
+  ...(open && {
+    marginLeft: drawerWidth,
+    width: `calc(100% - ${drawerWidth}px)`,
+    transition: theme.transitions.create(["width", "margin"], {
       easing: theme.transitions.easing.sharp,
-      duration: theme.transitions.duration.leavingScreen,
+      duration: theme.transitions.duration.enteringScreen,
     }),
-    ...(open && {
-      marginLeft: drawerWidth,
-      width: `calc(100% - ${drawerWidth}px)`,
-      transition: theme.transitions.create(['width', 'margin'], {
-        easing: theme.transitions.easing.sharp,
-        duration: theme.transitions.duration.enteringScreen,
-      }),
-    }),
-  }));
+  }),
+}));
 
+function Sidebar() {
+  const [open, setOpen] = React.useState(true);
+  const toggleDrawer = () => {
+    setOpen(!open);
+  };
+  const navigate = useNavigate();
 
-function Sidebar(){ 
-    const [open, setOpen] = React.useState(true);
-    const toggleDrawer = () => {
-      setOpen(!open);
-    };
-    const navigate = useNavigate();
-
-    const logout = async () => {
-        try {
-            await signOut(auth);
-            navigate('/');
-        } catch (error) {
-            console.log(error.message);
-        }
-    };
-    return (
-        <ThemeProvider theme={defaultTheme}>
-<AppBar position="absolute" open={open} sx={{ backgroundColor: 'green' }}>
-          <Toolbar
+  const logout = async () => {
+    try {
+      await signOut(auth);
+      navigate("/");
+    } catch (error) {
+      console.log(error.message);
+    }
+  };
+  return (
+    <ThemeProvider theme={defaultTheme}>
+      <AppBar position="absolute" open={open} sx={{ backgroundColor: "green" }}>
+        <Toolbar
+          sx={{
+            pr: "24px", // keep right padding when drawer closed
+          }}
+        >
+          <IconButton
+            edge="start"
+            color="inherit"
+            aria-label="open drawer"
+            onClick={toggleDrawer}
             sx={{
-              pr: '24px', // keep right padding when drawer closed
+              marginRight: "36px",
+              ...(open && { display: "none" }),
             }}
           >
-            <IconButton
-              edge="start"
-              color="inherit"
-              aria-label="open drawer"
-              onClick={toggleDrawer}
-              sx={{
-                marginRight: '36px',
-                ...(open && { display: 'none' }),
-              }}
-            >
-              <MenuIcon />
-            </IconButton>
+            <MenuIcon />
+          </IconButton>
+          <Typography
+            component="h1"
+            variant="h6"
+            color="inherit"
+            noWrap
+            sx={{ flexGrow: 1 }}
+          >
+            Deeplant
+          </Typography>
+          <IconButton color="inherit" component={Link} to="/profile">
+            <Badge color="secondary">
+              <PersonIcon />
+            </Badge>
             <Typography
-              component="h1"
-              variant="h6"
-              color="inherit"
-              noWrap
-              sx={{ flexGrow: 1 }}
-            >
-              Deeplant
-            </Typography>
-            <IconButton color="inherit" component={Link} to="/profile">
-              <Badge color="secondary">
-                <PersonIcon />
-              </Badge>
-              <Typography
               component="h1"
               variant="h6"
               color="inherit"
@@ -195,13 +197,13 @@ function Sidebar(){
             >
               프로필
             </Typography>
-            </IconButton>
+          </IconButton>
 
-            <IconButton color="inherit" onClick={logout}>
-              <Badge color="secondary">
-                <LogoutIcon />
-              </Badge>
-              <Typography
+          <IconButton color="inherit" onClick={logout}>
+            <Badge color="secondary">
+              <LogoutIcon />
+            </Badge>
+            <Typography
               component="h1"
               variant="h6"
               color="inherit"
@@ -210,34 +212,32 @@ function Sidebar(){
             >
               로그아웃
             </Typography>
-            </IconButton>
-            
-          </Toolbar>
-        </AppBar>
-       
-        <Drawer variant="permanent" open={open}>
-          <Toolbar
-            sx={{
-              display: 'flex',
-              alignItems: 'center',
-              justifyContent: 'flex-end',
-              px: [1],
-            }}
-          >
-            <IconButton onClick={toggleDrawer}>
-              <ChevronLeftIcon />
-            </IconButton>
-          </Toolbar>
-          <Divider />
-          <List component="nav">
-            {mainListItems}
-            <Divider sx={{ my: 1 }} />
-            {secondaryListItems}
-          </List>
-        </Drawer>
+          </IconButton>
+        </Toolbar>
+      </AppBar>
 
-        </ThemeProvider>
-    );
+      <Drawer variant="permanent" open={open}>
+        <Toolbar
+          sx={{
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "flex-end",
+            px: [1],
+          }}
+        >
+          <IconButton onClick={toggleDrawer}>
+            <ChevronLeftIcon />
+          </IconButton>
+        </Toolbar>
+        <Divider />
+        <List component="nav">
+          {mainListItems}
+          <Divider sx={{ my: 1 }} />
+          {secondaryListItems}
+        </List>
+      </Drawer>
+    </ThemeProvider>
+  );
 }
 
 Sidebar.propTypes={
--- Backend/frontend/src/components/User/UserList.js
+++ Backend/frontend/src/components/User/UserList.js
@@ -10,7 +10,6 @@ import Typography from "@mui/material/Typography";
 import { db } from "../../firebase-config";
 import { collection, getDocs } from "firebase/firestore";
 import { ButtonGroup } from "react-bootstrap";
-import Card from "react-bootstrap/Card";
 import Table from "@mui/material/Table";
 import TableBody from "@mui/material/TableBody";
 import TableCell from "@mui/material/TableCell";
@@ -18,13 +17,17 @@ import TableHead from "@mui/material/TableHead";
 import TableRow from "@mui/material/TableRow";
 import Grid from "@mui/material/Grid";
 import Paper from "@mui/material/Paper";
-import { styled, createTheme, ThemeProvider } from "@mui/material/styles";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
 import Box from "@mui/material/Box";
-import MuiDrawer from "@mui/material/Drawer";
-import MuiAppBar from "@mui/material/AppBar";
 import Toolbar from "@mui/material/Toolbar";
 import Container from "@mui/material/Container";
 import Sidebar from "../Base/Sidebar";
+import InputBase from "@mui/material/InputBase";
+import Divider from "@mui/material/Divider";
+import IconButton from "@mui/material/IconButton";
+import MenuIcon from "@mui/icons-material/Menu";
+import SearchIcon from "@mui/icons-material/Search";
+import DirectionsIcon from "@mui/icons-material/Directions";
 
 function UserList() {
   const [registerShow, setRegisterShow] = useState(false);
@@ -37,25 +40,132 @@ function UserList() {
   const handleEditClose = () => setEditShow(false);
   const users1CollectionRef = collection(db, "users_1");
   const users2CollectionRef = collection(db, "users_2");
+  const [searchedUsers1, setSearchedUsers1] = useState([]);
+  const [searchedUsers2, setSearchedUsers2] = useState([]);
+
   const handleEditShow = (user) => {
     setSelectedUser(user);
     setEditShow(true);
   };
   useEffect(() => {
-    const getUsers1 = async () => {
-      const data = await getDocs(users1CollectionRef);
-      setUsers1(data.docs.map((doc) => ({ ...doc.data(), id: doc.id })));
+    const fetchData = async () => {
+      const data1 = await getDocs(users1CollectionRef);
+      const data2 = await getDocs(users2CollectionRef);
+
+      setUsers1(data1.docs.map((doc) => ({ ...doc.data(), id: doc.id })));
+      setUsers2(data2.docs.map((doc) => ({ ...doc.data(), id: doc.id })));
+      setSearchedUsers1([
+        ...data1.docs.map((doc) => ({ ...doc.data(), id: doc.id })),
+      ]);
+      setSearchedUsers2([
+        ...data2.docs.map((doc) => ({ ...doc.data(), id: doc.id })),
+      ]);
     };
-    getUsers1();
+
+    fetchData();
   }, []);
 
-  useEffect(() => {
-    const getUsers2 = async () => {
-      const data = await getDocs(users2CollectionRef);
-      setUsers2(data.docs.map((doc) => ({ ...doc.data(), id: doc.id })));
-    };
-    getUsers2();
-  }, []);
+  const handleSearch = (event, userType) => {
+    const keyword = event.target.value.toLowerCase();
+    if (keyword === "") {
+      if (userType === "사용자1") {
+        setSearchedUsers1([...users1]);
+      } else if (userType === "사용자2") {
+        setSearchedUsers2([...users2]);
+      }
+    } else {
+      let results = [];
+      if (userType === "사용자1") {
+        results = users1.filter((user) => {
+          return (
+            user.name.toLowerCase().includes(keyword) ||
+            user.id.toLowerCase().includes(keyword)
+          );
+        });
+        setSearchedUsers1(results);
+      } else if (userType === "사용자2") {
+        results = users2.filter((user) => {
+          return (
+            user.name.toLowerCase().includes(keyword) ||
+            user.id.toLowerCase().includes(keyword)
+          );
+        });
+        setSearchedUsers2(results);
+      }
+    }
+  };
+  const renderUserTable = (users, userType) => (
+    <Grid item xs={6}>
+      <Paper sx={{ p: 2, display: "flex", flexDirection: "column" }}>
+        <Typography component="h2" variant="h6" color="primary" gutterBottom>
+          {userType}
+        </Typography>
+        <Paper
+          component="form"
+          sx={{
+            p: "2px 4px",
+            display: "flex",
+            alignItems: "center",
+          }}
+        >
+          <InputBase
+            sx={{ ml: 1, flex: 1 }}
+            placeholder="사용자 검색"
+            onChange={(event) => handleSearch(event, userType)}
+          />
+          <IconButton type="button" sx={{ p: "10px" }} aria-label="search">
+            <SearchIcon />
+          </IconButton>
+        </Paper>
+        <Table>
+          <TableHead>
+            <TableRow>
+              <TableCell>이름</TableCell>
+              <TableCell>아이디</TableCell>
+              <TableCell>권한</TableCell>
+              <TableCell>관리</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {users.map((user) => (
+              <TableRow key={user.id}>
+                <TableCell>{user.name}</TableCell>
+                <TableCell>{user.id}</TableCell>
+                <TableCell>{userType}</TableCell>
+                <TableCell>
+                  <Button
+                    variant="success"
+                    onClick={() => handleEditShow(user)}
+                  >
+                    수정
+                  </Button>
+                  <Modal
+                    show={editShow}
+                    onHide={handleEditClose}
+                    backdrop="static"
+                    keyboard={false}
+                    centered
+                  >
+                    <Modal.Header closeButton>
+                      <Modal.Title>회원 정보 수정</Modal.Title>
+                    </Modal.Header>
+                    <Modal.Body>
+                      <UserEdit selectedUser={selectedUser} />
+                    </Modal.Body>
+                    <Modal.Footer>
+                      <Button variant="primary" onClick={handleEditClose}>
+                        저장
+                      </Button>
+                    </Modal.Footer>
+                  </Modal>
+                </TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </Paper>
+    </Grid>
+  );
   const defaultTheme = createTheme();
 
   return (
@@ -134,135 +244,8 @@ function UserList() {
                   .
                 </Paper>
               </Grid>
-              <Grid item xs={6}>
-                <Paper
-                  sx={{
-                    p: 2,
-                    display: "flex",
-                    flexDirection: "column",
-                  }}
-                >
-                  <Typography
-                    component="h2"
-                    variant="h6"
-                    color="primary"
-                    gutterBottom
-                  >
-                    사용자1
-                  </Typography>
-                  <Table>
-                    <TableHead>
-                      <TableRow>
-                        <TableCell>이름</TableCell>
-                        <TableCell>아이디</TableCell>
-                        <TableCell>권한</TableCell>
-                        <TableCell>관리</TableCell>
-                      </TableRow>
-                    </TableHead>
-                    <TableBody>
-                      {users1.map((user1) => (
-                        <TableRow key={user1.id}>
-                          <TableCell>{user1.name}</TableCell>
-                          <TableCell>{user1.id}</TableCell>
-                          <TableCell>사용자1</TableCell>
-                          <TableCell>
-                            <Button
-                              variant="success"
-                              onClick={() => handleEditShow(user1)}
-                            >
-                              수정
-                            </Button>
-                            <Modal
-                              show={editShow}
-                              onHide={handleEditClose}
-                              backdrop="static"
-                              keyboard={false}
-                              centered
-                            >
-                              <Modal.Header closeButton>
-                                <Modal.Title>회원 정보 수정</Modal.Title>
-                              </Modal.Header>
-                              <Modal.Body>
-                                <UserEdit selectedUser={selectedUser} />
-                              </Modal.Body>
-                              <Modal.Footer>
-                                <Button
-                                  variant="primary"
-                                  onClick={handleEditClose}
-                                >
-                                  저장
-                                </Button>
-                              </Modal.Footer>
-                            </Modal>
-                          </TableCell>
-                        </TableRow>
-                      ))}
-                    </TableBody>
-                  </Table>
-                </Paper>
-              </Grid>
-              {/* Recent Orders */}
-              <Grid item xs={6}>
-                <Paper sx={{ p: 2, display: "flex", flexDirection: "column" }}>
-                  <Typography
-                    component="h2"
-                    variant="h6"
-                    color="primary"
-                    gutterBottom
-                  >
-                    사용자2
-                  </Typography>
-                  <Table>
-                    <TableHead>
-                      <TableRow>
-                        <TableCell>이름</TableCell>
-                        <TableCell>아이디</TableCell>
-                        <TableCell>권한</TableCell>
-                        <TableCell>관리</TableCell>
-                      </TableRow>
-                    </TableHead>
-                    <TableBody>
-                      {users2.map((user2) => (
-                        <TableRow key={user2.id}>
-                          <TableCell>{user2.name}</TableCell>
-                          <TableCell>{user2.id}</TableCell>
-                          <TableCell>사용자2</TableCell>
-                          <TableCell>
-                            <Button
-                              variant="success"
-                              onClick={() => handleEditShow(user2)}
-                            >
-                              수정
-                            </Button>
-                            <Modal
-                              show={editShow}
-                              onHide={handleEditClose}
-                              backdrop="static"
-                              keyboard={false}
-                              centered
-                            >
-                              <Modal.Header closeButton>
-                                <Modal.Title>회원 정보 수정</Modal.Title>
-                              </Modal.Header>
-                              <Modal.Body>
-                                <UserEdit selectedUser={selectedUser} />
-                              </Modal.Body>
-                              <Modal.Footer>
-                                <Button
-                                  variant="primary"
-                                  onClick={handleEditClose}
-                                >
-                                  저장
-                                </Button>
-                              </Modal.Footer>
-                            </Modal>
-                          </TableCell>
-                        </TableRow>
-                      ))}
-                    </TableBody>
-                  </Table>
-                </Paper>
-              </Grid>
+              {renderUserTable(searchedUsers1, "사용자1")}
+              {renderUserTable(searchedUsers2, "사용자2")}
             </Grid>
           </Container>
         </Box>
