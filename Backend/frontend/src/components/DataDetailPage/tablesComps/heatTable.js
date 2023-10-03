97669798c27a9979474cfec1f61365fb75376abc
clapsh <psh8295015@gmail.com>
2023-10-03 13:30:54 +0900

    Fix rejectedDataList UI

--- Backend/frontend/src/components/DataDetailPage/tablesComps/heatTable.js
+++ Backend/frontend/src/components/DataDetailPage/tablesComps/heatTable.js
@@ -2,8 +2,8 @@ import { Paper,  Table, TableBody, TableCell, TableContainer, TableHead, TableRo
 
 const HeatTable=({edited, heatInput, heated_data,toggle3Value,handleInputChange,})=>{
     return(
-        <TableContainer key='heatedmeat' component={Paper} sx={{minWidth:'fitContent',maxWidth:'680px',overflow:'auto'}}>
-            <Table sx={{ minWidth: 650 }} size="small" aria-label="a dense table">
+        <TableContainer key='heatedmeat' component={Paper} sx={{/*minWidth:'fit-content',maxWidth:'680px',*/width:'100%',overflow:'auto'}}>
+            <Table sx={{ minWidth: 300 }} size="small" aria-label="a dense table">
                 <TableHead>
                     <TableRow key={'heatedmeat-explanation'}>
                         <TableCell key={'heatedmeat-exp-col'}>{}</TableCell>
